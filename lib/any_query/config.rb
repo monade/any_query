4637da7abc379ef4417a49bb8e87e837920b2029
Piero Dotti <progiemmeh@gmail.com>
2023-09-29 11:46:09 +0200

    fix(config): logger not overwritable

--- lib/any_query/config.rb
+++ lib/any_query/config.rb
@@ -3,10 +3,12 @@
 module AnyQuery
   # A class to handle configuration for AnyQuery
   class Config
-    attr_writer :logger
-
     def self.logger
       @logger ||= Logger.new($stdout)
+    end
+
+    def self.logger=(logger)
+      @logger = logger
     end
   end
 end
